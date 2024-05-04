4595a14a30a6df8c17c6909bbf12ca2e1320d6de
Ankur Atri <ankuratri.in@gmail.com>
2024-05-04 16:06:18 +0530

    Query variables and resolvers added

--- backend/resolver.js
+++ backend/resolver.js
@@ -2,8 +2,23 @@ import db from "./_db.js";
 
 export const resolvers = {
   Query: {
-    games: () => db.games,
-    reviews: () => db.reviews,
-    authors: () => db.authors,
+    games: () => {
+      return db.games;
+    },
+    game: (_, args) => {
+      return db.games.find((game) => game.id === args.id);
+    },
+    reviews: () => {
+      return db.reviews;
+    },
+    review: (_, args) => {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    authors: () => {
+      return db.authors;
+    },
+    author: (_, args) => {
+      return db.authors.find((author) => author.id === args.id);
+    },
   },
 };
