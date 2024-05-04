b72838d2ad7cba7880da44d84f57e9d5f1f00687
Ankur Atri <ankuratri.in@gmail.com>
2024-05-04 13:29:07 +0530

    Schema & Types added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
 
 // The ApolloServer constructor requires two parameters:
 // your schema definition and your set of resolvers.
 const server = new ApolloServer({
-  //   typeDefs,
+  typeDefs,
   //   resolvers,
 });
 
